fda9e6b3c6a9fd1b35ee24cb5900f6b14f5cf612
j1241 <j1241>
2016-09-28 11:12:05 +0900

    ruby講座

---
+++ Vector.rb
@@ -0,0 +1,23 @@
+def new(x, y)
+  print "x座標を入力してください"
+  x = gets.to_i
+  print "y座標を入力してください"
+  y = gets.to_i
+
+  return(x, y)
+end
+
+def to_s(x, y)
+
+end
+
+def length(x, y)
+
+end
+
+class Vector
+  x = 0
+  y = 0
+
+  Klass::new(x, y)
+end
